136357b37679425d97129dae3b4b15a3c1a74485
Aleksandra Volkova <91-ksandra@mail.ru>
2024-08-09 14:50:57 +0600

    Create DB config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "hexlet.code"
@@ -25,6 +26,9 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
